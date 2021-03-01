15d75f75227504f8029944863d7fe43e3d04c416
lai Ben <waterblue104@gmail.com>
2021-03-01 22:43:18 +0800

    layout

---
+++ src/components/Contact.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+#contact.contact
+  .container
+    p.description Hi I&apos;m Ben I&apos;m a Frontend Developer
+    h2 CONTACT
+
+    .skill
+      .subtitle SKILLS
+      p
+        | JavaScript / Vue.js(Vuex) / HTML5(pug) / CSS3(SCSS/SASS) / RWD / Git Flow / Canvas / SVG / GSAP(TweenMax) / Bootstrap /
+        | JQuery
+        br
+        br
+        | Learning ( PIXI.js &#x3001; Three.js &#x3001; UX/UI concept &#x3001; WebGL )
+
+    .info
+      .subtitle CONTACT
+      .info__email
+        a(href='mailto:waterblue104@gmail.com') Email : waterblue104@gmail.com
+      .info__phone
+        a(href='tel:0981896398') Phone : 0981896398
+
+    .social-bar
+      a.fab(v-for="item in iconLinks" :href='item.link' target='_blank')
+        font-awesome-icon(:icon="['fab',`${item.icon}`]")
+
+    .footer
+      .container
+        p.copyright Copyright @ 2020 Ben All right reserved
+
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      iconLinks: [
+        {
+          link: 'https://github.com/livoist',
+          icon: 'github'
+        },
+        {
+          link: 'https://www.instagram.com/waterblue104/',
+          icon: 'instagram'
+        },
+        {
+          link: 'https://medium.com/@waterblue104',
+          icon: 'medium'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .header
+    .navbar
+      a.logo(href="javascript:void('0')") B
+      .nav.u-z-index_7
+        //-a.menu-toggle(href='#popup-overlay')
+          span
+
+        #popup-overlay.popup-menu
+          a.popup-menu__close.u-z-index_7(href='#!')
+            span
+            span
+
+          .bar-container.bg-black.u-px-30
+            ul.u-pt-100
+              li(v-for="item in headerLinks")
+                a.popup-menu__item(:href='`#${ item.title }`') {{ item.title.toUpperCase() }}
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      headerLinks: [
+        {
+          title: 'about',
+        },
+        {
+          title: 'portfolio'
+        },
+        {
+          title: 'contact'
+        }
+      ]
+    }
+  },
+  methods: {
+    scrollTarget(target) {
+      window.scroll({
+        top: target,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
